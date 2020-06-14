653144daac01a1ec99c8be3d10d204944c62d683
Tim Jones <tdjones74021@yahoo.com>
2020-06-14 14:12:41 -0500

    Basic updates to support use of MySQL via SQLAlchemy with Flask.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,30 @@
 # app/__init__.py
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import app_config
+
+# Database initialization
+db = SQLAlchemy()
 
 # Application initialization
-app = Flask(__name__, instance_relative_config = True)
+def create_app(config_name):
+	app = Flask(__name__, instance_relative_config = True)
+	# Load configuration
+	app.config.from_object(app_config[config_name])
+	app.config.from_pyfile('config.py')
+	db.init_app(app)
+
+	# Temporary route
+	@app.route("/")
+	def hello_world():
+		return "Hello, world!"
+
+	return app
+
 
 # Load the views
-from app import views
+# from app import views
 
-# Load configuration
-app.config.from_object('config')
+
--- config.py
+++ config.py
@@ -1,4 +1,29 @@
 # config.py
 
-# Enable Flask debug mode
-DEBUG = True
+class Config(object):
+	"""
+	Common configuration parameters
+	"""
+
+	# Put any configuration information common to all environments
+
+class DevelopmentConfig(Config):
+	"""
+	Development configuration parameters
+	"""
+
+	# Enable Flask debug mode
+	DEBUG = True
+	SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+	"""
+	Production configuration parameters
+	"""
+
+	DEBUG = False
+
+app_config = {
+	'development': DevelopmentConfig,
+	'production': ProductionConfig
+}
