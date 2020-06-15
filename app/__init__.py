416f017e4324e6eec56c2c8fbcfda55226d7643c
Tim Jones <tdjones74021@yahoo.com>
2020-06-15 15:31:38 -0500

    Add SQLAlchemy models and DB migrations for 'AppUser' and 'Bookmark' tables.

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 from config import app_config
 
 # Database initialization
 db = SQLAlchemy()
+
+login_manager = LoginManager()
 
 # Application initialization
 def create_app(config_name):
@@ -15,6 +19,14 @@ def create_app(config_name):
 	app.config.from_object(app_config[config_name])
 	app.config.from_pyfile('config.py')
 	db.init_app(app)
+
+	login_manager.init_app(app)
+	login_manager.login_message = "Please log in to access this page."
+	login_manager.login_view = "auth.login"
+
+	migrate = Migrate(app, db)
+
+	from app import models
 
 	# Temporary route
 	@app.route("/")
